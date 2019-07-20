7467fc64f30509df35a8cf8e321e274161809286
yaobin1107 <1059843714@qq.com>
2019-07-20 21:44:37 +0800

    基本框架

---
+++ dreamingcourse/src/main/java/cn/ykt/dreamingcourse/DreamingcourseApplication.java
@@ -0,0 +1,13 @@
+package cn.ykt.dreamingcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamingcourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamingcourseApplication.class, args);
+    }
+
+}
